40ce34b983b5c424ddd6ffb09d96a04ca60b167e
WilsonSanFullStack <clinicasystemlab@gmail.com>
2023-11-27 19:58:21 -0500

    probando

--- src/app/pages/dashboar/user/[id]/page.jsx
+++ src/app/pages/dashboar/user/[id]/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 async function getUserId(id) {
